b545075e75b920a6ab535cdef07f82fbd6bc4e6a
ueshou <53548215+ueshou@users.noreply.github.com>
2021-01-22 13:08:14 +0900

    Update twice.py

--- mypkg/scripts/twice.py
+++ mypkg/scripts/twice.py
@@ -12,6 +12,7 @@ rospy.init_node('twice')
 sub = rospy.Subscriber('count_up', Int32, cb)
 pub = rospy.Publisher('twice', Int32, queue_size=1)
 rate = rospy.Rate(10)
+
 while not rospy.is_shutdown():
     pub.publish(n)
     rate.sleep()
